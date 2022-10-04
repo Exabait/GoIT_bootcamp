b339ebe0858af16dd1b90a2bd6e154065c1777a8
Vsevolod Stepanenko <v.step2327@gmail.com>
2022-10-04 13:01:44 +0300

    lesson2 - result

--- js/cycles.js
+++ js/cycles.js
@@ -14,7 +14,13 @@
 /*
 ? Виведіть числа від 1 до 50
 */
+// let number = 1;
 
+// while (number <= 50) {
+  // console.log(++number);
+  // number = number + 1;
+  // number++
+// }
 
 /*
 ? Знайдіть суму чисел від 1 до 100
@@ -43,6 +49,16 @@ result = 6;
 ? Яке число вийде?
 ? Порахуйте кількість ітерацій, необхідних для цього, та запишіть його в змінну num.
 */
+// let number = 1000;
+// let iterator = 1;
+
+// console.log(++iterator);
+
+// while (number >= 50 && iterator <= 4) {
+//   console.log(number, iterator);
+//   number /= 2;
+//   iterator++;
+// }
 
 /*
  * Do While
@@ -58,6 +74,12 @@ result = 6;
 ? Передбачається, що відвідувач вводить лише числа;
 */
 
+// let number;
+//
+// do {
+//   number = +prompt('Введіть якесь число');
+// } while (number < 100);
+
 /*
  * For
  */
@@ -65,8 +87,10 @@ result = 6;
 //   statements
 // }
 
-// for (let i = 0; i < 10; i += 1) {
-//   console.log(i);
+// const test = 'За допомогою циклу for виведіть парні числа від 2 до 10.';
+//
+// for (let i = 0; i < test.length; i++) {
+//   console.log(test[i]);
 // }
 
 /*
@@ -74,20 +98,20 @@ result = 6;
 */
 
 // for (let i = 2; i <= 10; i += 1) {
-//   if (i % 2 === 0) {
-//     console.log(i);
-//   }
-
-//   // if (!(i % 2)) {
+//   // if (i % 2 === 0) {
 //   //   console.log(i);
 //   // }
+//
+//   if (!(i % 2)) {
+//     console.log(i);
+//   }
 // }
 
 // for (let i = 2; i <= 10; i += 1) {
 //   if (i % 2 !== 0) {
 //     continue;
 //   }
-
+//
 //   console.log(i);
 // }
 
@@ -95,12 +119,82 @@ result = 6;
 ? Напишіть цикл на 10 ітерацій, який завершується, щойно лічильник циклу досягне значення 5
 */
 
+// for (let i = 1; i <= 10; i += 1) {
+//   if (i === 5) {
+//     break;
+//   }
+//
+//   console.log(i);
+// }
+
 /*
 ? Напишіть програму, яка виводить через console.log усі числа від 1 до 100, з двома винятками.
 ? Для чисел, що націло діляться на 3, вона повинна виводити 'Fizz',
 ? а для чисел, що діляться на 5 – 'Buzz'.
 ? Для чисел які кратні 3 та 5 = 'FizzBuzz'
 */
+
+// перебрати всі числа від 1 до 100
+// перевірити, чи ділить на 3
+
+// Option 1
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('FizzBuzz');
+//   } else if (i % 3 === 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 === 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// Option 2
+// for (let i = 1; i <= 100; i++) {
+//   let str = '';
+//
+//   if (i % 3 === 0) {
+//     str = 'Fizz';
+//   }
+//
+//   if (i % 5 === 0) {
+//     str += 'Buzz';
+//   }
+//
+//   console.log(str || i);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//   let str = '';
+
+  // switch (true) {
+  //   case i % 3 === 0: {
+  //     str = 'Fizz';
+  //     if (i % 5 === 0) {
+  //       str += 'Buzz';
+  //     }
+  //     break;
+  //   }
+  //   case i % 5 === 0: {
+  //     str += 'Buzz';
+  //     break;
+  //   }
+  //   default: {
+  //
+  //   }
+  // }
+
+  // if (i % 3 === 0) {
+  //   str = 'Fizz';
+  // }
+  //
+  // if (i % 5 === 0) {
+  //   str += 'Buzz';
+  // }
+
+//   console.log(str || i);
+// }
 
 /*
  ? Виведіть методом console.log() зірочки від 1 до 7 штук у вигляді трикутника таким чином:
@@ -112,3 +206,28 @@ result = 6;
  ? ******
  ? *******
  */
+
+// let stars = '*';
+//
+// for (let i = 1; i <= 7; i++) {
+//   console.log(stars);
+//   stars += '*';
+// }
+
+// if (i / i === 1 && i / 1 === i) {
+//   console.log(i);
+// }
+
+
+// Виведіть прості числа в діапазоні від 1 до 100;
+for (let i = 1; i <= 100; i++) {
+  for (let j = 1; j <= i; j++) {
+    if (j === i) {
+      console.log(i);
+    } else if (i % j !== 0) {
+      continue;
+    } else {
+      break;
+    }
+  }
+}
--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@
 // тіло if
 // }
 
-// if (number < 20) {
+// const number = 0;
+//
+// if (true) {
 //   console.log('Ми потрапили до if');
 // }
 
@@ -17,11 +19,27 @@
 ? Якщо це так - виведіть 'так', інакше виведіть 'ні'.
 */
 
+// const str = 'abcde';
+//
+// if (str[0] === 'b') {
+//   console.log('yes');
+// } else {
+//   console.log('no');
+// }
+
 
 /*
 ? Запросіть у користувача число, якщо число дорівнює 10,
 ? то виведіть 'Вірно', інакше виведіть 'Невірно'.
 */
+
+// const number = Number.parseInt(prompt('Enter some number'));
+//
+// if (number === 10) {
+//   console.log('yes');
+// } else {
+//   console.log('no');
+// }
 
 
 /*
@@ -33,7 +51,41 @@
 ? [45 до 60) - четверта чверть години
 */
 
-// const min = 48;
+// const min = 32;
+//
+// if (min >= 0 && min < 15) {
+//   console.log('перша чверть години');
+// } else if (min >= 15 && min < 30) {
+//   console.log('друга чверть години');
+// } else if (min >= 30 && min < 45) {
+//   console.log('третя чверть години');
+// } else if (min >= 45 && min < 60) {
+//   console.log('четверта чверть години');
+// } else {
+//   console.log('Number is invalid');
+// }
+
+// switch (true) {
+//   case min >= 0 && min < 15: {
+//     console.log('перша чверть години');
+//     break;
+//   }
+//   case min >= 15 && min < 30: {
+//     console.log('перша чверть години');
+//     break;
+//   }
+//   case min >= 30 && min < 45: {
+//     console.log('перша чверть години');
+//     break;
+//   }
+//   case min >= 45 && min < 60: {
+//     console.log('перша чверть години');
+//     break;
+//   }
+//   default: {
+//     console.log('Number is invalid');
+//   }
+// }
 
 /*
 ? Напишіть код, який запитуватиме:
@@ -41,6 +93,14 @@
 ? Якщо користувач вводить ECMAScript,
 ? то показати: «Вірно!», інакше – відобразити: «Не знаєте? ECMAScript!»
 */
+
+// const answer = prompt('Яка «офіційна» назва JavaScript?', 'Test').toLowerCase();
+//
+// if (answer === 'ECMAScript'.toLowerCase()) {
+//   console.log('ECMAScript');
+// } else {
+//   console.log('Не знаєте? ECMAScript!');
+// }
 
 
 /*
@@ -50,6 +110,18 @@
 ? 0, якщо значення дорівнює нулю.
 ? Передбачається, що користувач вводить лише цифри.
 */
+
+// const number = Number.parseInt(prompt('Enter some number'));
+//
+// if (number > 0) {
+//   console.log('1');
+// } else if (number < 0) {
+//   console.log('-1');
+// } else if (number === 0) {
+//   console.log('0')
+// } else {
+//   console.log('Number is invalid');
+// }
 
 /*
  * Switch case
@@ -68,24 +140,24 @@
 //     інструкції;
 // }
 
-// const number = 0;
-
+// const number = 20;
+//
 // switch (number) {
 //   case 20: {
 //     console.log('Число 20');
 //     break;
 //   }
-
+//
 //   case 10: {
 //     console.log('Число 10');
 //     break;
 //   }
-
+//
 //   case 40: {
 //     console.log('Число 40');
 //     break;
 //   }
-
+//
 //   default: {
 //     console.log('Такого числа немає');
 //   }
@@ -107,6 +179,28 @@
 //   alert('Ви ввели якесь інше число');
 // }
 
+// switch (number) {
+//   case 0: {
+//     alert('Ви ввели число 0');
+//     break;
+//   }
+//   case 1: {
+//     alert('Ви ввели число 1');
+//     break;
+//   }
+//   case 2: {
+//     alert('Ви ввели число 2, а може і 3');
+//     break;
+//   }
+//   case 3: {
+//     alert('Ви ввели число 2, а може і 3');
+//     break;
+//   }
+//   default: {
+//     alert('Ви ввели якесь інше число');
+//   }
+// }
+
 /*
  * Тернарний оператор
  */
@@ -117,7 +211,11 @@
 ? Перепишіть конструкцію if з використанням умовного оператора '?':
 */
 // const a = 2;
-// const b = 1;
+// const b = 5;
+
+// let result = a + b < 4 ? 'Мало' : 'Багато';
+
+// a + b < 4 || console.log('Багато');
 
 // if (a + b < 4) {
 //   result = 'Мало';
@@ -132,12 +230,16 @@
 */
 
 // const login = 'Директор';
+// let message = login === 'Співробітник' ? 'Привіт' :
+//   login === 'Директор' ? 'Вітаю' :
+//   login === '' ? 'Немає логіну' :
+//   '';
 
 // if (login === 'Співробітник') {
 //   message = 'Привіт';
-// } else if (login == 'Директор') {
+// } else if (login === 'Директор') {
 //   message = 'Вітаю';
-// } else if (login == '') {
+// } else if (login === '') {
 //   message = 'Немає логіну';
 // } else {
 //   message = '';
